8cb084d1d3d4b46b4d6ed2b156db818155c9dd9a
WambiruL <wambirulorraine@gmail.com>
2023-12-28 20:27:28 +0300

    useState Hook to display blogs

--- src/Home.js
+++ src/Home.js
@@ -1,23 +1,36 @@
 import { useState } from "react";
 const Home = () => {
+    const [blogs, setBlogs]= useState([
+        {title: 'My new Website', body: 'lorem ipsum...', author:'mario', id: 1},
+        {title: 'Welcome Party', body: 'lorem ipsum...', author:'luigi', id: 2},
+        {title: 'Web dev top tips', body: 'lorem ipsum...', author:'yoshi', id: 3}
+    ]);
 
 
     // let name= "Mario";
-    const[name, setName]=useState('mario'); //setName is the name that will replace Mario
-    const[age, setAge]=useState(25);
-    const handleClick= (e)=>{
-        console.log("Hello World", e);
-        setName('luigi');
-        setAge(30)
+    // const[name, setName]=useState('mario'); //setName is the name that will replace Mario
+    // const[age, setAge]=useState(25);
+    // const handleClick= (e)=>{
+    //     console.log("Hello World", e);
+        // setName('luigi');
+        // setAge(30)
         //when the button is clicked it will change the name Mario to Luigi
-    }
+    
 
     return (
         //use state hook makes a value reactive
         <div className="home">
-            <h2>Homepage</h2>
+            {blogs.map((blog)=>(
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{blog.title}</h2>
+                    <p>Written by: {blog.author}</p>
+
+                </div>
+
+            ))}
+            {/* <h2>Homepage</h2>
             <p>{name} is {age} years old</p>
-            <button onClick={handleClick}>Click Me</button>
+            <button onClick={handleClick}>Click Me</button> */}
         </div>
 
     );
