22697d44713bc925057897d8e9028c6098e4d5d7
WambiruL <wambirulorraine@gmail.com>
2024-01-02 12:58:20 +0300

    Install react router dom for routing multiple pages

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import Navbar from './Navbar';
 import Home from './Home';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 
 function App() {
 
   return (
-    <div className="App">
-      <Navbar></Navbar>
-      <div className="content">
-       <Home></Home>        
+    <Router>
+      <div className="App">
+        <Navbar></Navbar>
+        <div className="content">
+          <Switch> {/*All routes go here */}
+            <Route path="/"> {/*for the home page */}
+              <Home></Home>
+            </Route>
+          </Switch>        
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
